70429abb5f9143b3162df528ccca9dfd5b544a9f
chuanqin <chuanqing.qin@nokia-sbell.com>
2021-03-18 09:25:11 +0800

    add test uitility pytest support

---
+++ tst/test_ocr.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python3
+import pytest
+from cnocr import CnOcr
+import os
+
+# to demo how to ocr one picture
+def test_ocr():
+    test_file = r'../res/test.png'
+    assert(os.path.exists(test_file))
+    ocr = CnOcr()
+    res = ocr.ocr(test_file)
+    print("Predicted Chars:", res)
+    for line in res:
+        print(''.join(line))
+
+    assert res != None
+    assert len(res) == 13
---
+++ tst/test_screenshot.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python3
+
+# demo how to screenshot of the full size web page
+def test_screenshot():
+    None
